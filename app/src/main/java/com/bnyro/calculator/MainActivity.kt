50232e1392874d5577363fa7dbf767c007f66092
Bnyro <yannick.fehler@web.de>
2022-06-10 22:33:04 +0200

    toggle and animations

--- app/src/main/java/com/bnyro/calculator/MainActivity.kt
+++ app/src/main/java/com/bnyro/calculator/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.text.SpannableStringBuilder
 import android.view.View
 import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.view.animation.LinearInterpolator
 import android.view.animation.RotateAnimation
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bnyro.calculator.databinding.ActivityMainBinding
 import com.google.android.material.color.DynamicColors
+import java.text.DecimalFormat
 import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : AppCompatActivity() {
@@ -23,54 +23,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val rotate = RotateAnimation(
-            0F,
-            180F,
-            Animation.RELATIVE_TO_SELF,
-            0.5f,
-            Animation.RELATIVE_TO_SELF,
-            0.5f
-        )
-        rotate.apply {
-            duration = 600
-            fillAfter = true
-            interpolator = LinearInterpolator()
-        }
-
-        val rotateReverse = RotateAnimation(
-            180F,
-            360F,
-            Animation.RELATIVE_TO_SELF,
-            0.5f,
-            Animation.RELATIVE_TO_SELF,
-            0.5f
-        )
-        rotateReverse.apply {
-            duration = 600
-            fillAfter = true
-            interpolator = LinearInterpolator()
-        }
-
-        val animSlideDown = AnimationUtils.loadAnimation(
-            applicationContext, R.anim.slide_down
-        )
-
-        val animSlideUp = AnimationUtils.loadAnimation(
-            applicationContext, R.anim.slide_up
-        )
-
-        binding.arrow.setOnClickListener { _ ->
-            if (binding.advancedSecondRow.visibility == View.GONE) {
-                binding.advancedSecondRow.visibility = View.VISIBLE
-                binding.advancedThirdRow.visibility = View.VISIBLE
-                binding.arrow.startAnimation(rotate)
-            } else {
-                binding.advancedSecondRow.visibility = View.GONE
-                binding.advancedThirdRow.visibility = View.GONE
-                binding.arrow.startAnimation(rotateReverse)
-            }
-        }
-
         binding.input.showSoftInputOnFocus = false
     }
 
@@ -81,12 +33,12 @@ class MainActivity : AppCompatActivity() {
         val rightStr = oldStr.substring(cursorPos)
         binding.input.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr))
         binding.input.setSelection(cursorPos + strToAdd.length)
-        val resultString = result
+        val resultString = getResult()
         if (resultString != "NaN") binding.result.text = resultString
     }
 
     fun equalsBTN(view: View?) {
-        val resultString = result
+        val resultString = getResult()
         if (resultString != "NaN") {
             binding.input.setText(resultString)
             binding.input.setSelection(resultString.length)
@@ -108,21 +60,62 @@ class MainActivity : AppCompatActivity() {
             selection.replace(cursorPos - 1, cursorPos, "")
             binding.input.text = selection
             binding.input.setSelection(cursorPos - 1)
-            val resultString = result
+            val resultString = getResult()
             binding.result.text = if (resultString != "NaN") resultString else ""
         }
     }
 
-    private val result: String
-        get() {
-            val userExp = binding.input.text.toString()
-            val exp = Expression(userExp)
-            val result: Double = exp.calculate()
-            var resultString = result.toString()
-            if (resultString.endsWith(".0")) resultString =
-                resultString.substring(0, resultString.length - 2)
-            return resultString
+    fun calcToggle(view: View?) {
+        if (binding.calcToggle.visibility == View.GONE) {
+            val rotate = RotateAnimation(
+                0F,
+                180F,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
+            )
+            rotate.apply {
+                duration = 200
+                fillAfter = true
+                interpolator = LinearInterpolator()
+            }
+            binding.calcToggle.visibility = View.VISIBLE
+            binding.arrow.startAnimation(rotate)
+        } else {
+            val rotateReverse = RotateAnimation(
+                180F,
+                360F,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
+            )
+            rotateReverse.apply {
+                duration = 200
+                fillAfter = true
+                interpolator = LinearInterpolator()
+            }
+            binding.calcToggle.visibility = View.GONE
+            binding.arrow.startAnimation(rotateReverse)
         }
+    }
+
+    private fun getResult(): String {
+        val userExp = binding.input.text.toString()
+        val exp = Expression(userExp)
+        val resultTemp: Double = exp.calculate()
+        val result = roundTwoDecimals(resultTemp)
+        var resultString = result.toString()
+        if (resultString.endsWith(".0")) resultString =
+            resultString.substring(0, resultString.length - 2)
+        return resultString
+    }
+
+    private fun roundTwoDecimals(d: Double): Double {
+        val twoDForm = DecimalFormat("#.#####")
+        return java.lang.Double.valueOf(twoDForm.format(d))
+    }
 
     fun clearBTN(view: View?) {
         binding.input.setText("")
@@ -240,6 +233,18 @@ class MainActivity : AppCompatActivity() {
         updateText("tan(")
     }
 
+    fun arcSinBTN(view: View?) {
+        updateText("arcsin(")
+    }
+
+    fun arcCosBTN(view: View?) {
+        updateText("arccos(")
+    }
+
+    fun arcTanBTN(view: View?) {
+        updateText("arctan(")
+    }
+
     fun logBTN(view: View?) {
         updateText("log10(")
     }
@@ -260,10 +265,17 @@ class MainActivity : AppCompatActivity() {
         updateText("!")
     }
 
-    fun invBTN(view: View?) {
-    }
-
     fun absBTN(view: View?) {
         updateText("abs(")
+    }
+
+    fun invBTN(view: View?) {
+        binding.trigonometric.visibility = View.GONE
+        binding.trigonometricInverted.visibility = View.VISIBLE
+    }
+
+    fun invInvertedBTN(view: View?) {
+        binding.trigonometric.visibility = View.VISIBLE
+        binding.trigonometricInverted.visibility = View.GONE
     }
 }
